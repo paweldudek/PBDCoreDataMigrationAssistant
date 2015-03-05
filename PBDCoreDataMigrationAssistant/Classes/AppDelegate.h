955594a2d7cced9ab2ca4a20e4eb4d715cd6c191
Pawel Dudek <pawel@dudek.mobi>
2015-03-05 17:00:27 +0100

    Initial commit

---
+++ PBDCoreDataMigrationAssistant/Classes/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  PBDCoreDataMigrationAssistant
+//
+//  Created by Pawel Dudek on 05/03/15.
+//  Copyright (c) 2015 Dudek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
